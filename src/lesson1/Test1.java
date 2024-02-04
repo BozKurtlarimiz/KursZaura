d027836fdf73d98ed44c11fda3a561da0110c0ae
Эльшан <askerovelshan1990@gmail.com>
2024-02-04 17:06:28 +0300

    1

---
+++ src/lesson1/Test1.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("welcome to the first lesson!!!");
+        System.out.println("welcome to the first lesson!!!");
+        System.out.println("boroda");
+    }
+
+}
